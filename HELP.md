66ff13f5c47a52e63d4a1b59baa3d2d98b57d47d
张全 <72014072+gray-ray@users.noreply.github.com>
2023-11-30 16:55:52 +0800

    feat: swaggerUI文档加入

--- HELP.md
+++ HELP.md
@@ -3,3 +3,10 @@
 [](https://docs.nestjs.com/fundamentals/circular-dependency)
 forwardRef 相互依赖问题解决
 NOTE: service  module 都需要配置
+
+### 待办项
+
+- 接入 swagger ui
+- 全局过滤器
+- restful
+- 登录鉴权
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('用户管理系统')
+    .setDescription('用户、应用、角色、权限统一管理')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
